095a73fcb55fb3b80cff8bf4a013e580a17742ed
itsrv23 <nifirus@gmail.com>
2022-06-17 09:35:03 +0300

    Initial commit

---
+++ src/main/java/ru/itsrv23/hw/algoritm/Application.java
@@ -0,0 +1,13 @@
+package ru.itsrv23.hw.algoritm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
